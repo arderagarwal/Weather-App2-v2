e62617528839a4d5f6f7f8da569188b110b7c8e6
Adarsh Agarwal <adarshagarwal8@gmail.com>
2021-01-29 19:17:20 +0530

    Added templates with headers and footers

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
+const path = require('path')
 const express = require('express')
+const hbs = require('hbs')
+
 const app = express()
-app.get('',(request,response)=>{
-    response.send("Hello World")
+const publicPath = path.join(__dirname,'../public/')
+// app.use(express.static(publicPath))
+app.set('view engine','hbs')
+app.set('views',path.join(__dirname,'../templates/views'))
+hbs.registerPartials(path.join(__dirname,'../templates/partials'));
+app.get('',(req,res)=>{
+    res.render('index',{
+        title : "Weather App",
+        name: "Adarsh"
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title : "About",
+        name : "Adarsh"
+    })
+})
+app.get('*',(req,res)=>{
+    res.send("<h1>404 Page</h1>")
 })
 app.listen(8080,()=>{
     console.log('We are live on port 8080')
